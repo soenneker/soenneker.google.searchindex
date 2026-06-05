577b34ec1c476decabd0c652c5ead2336085073b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:36:53 -0500

    xml commenting

--- src/Soenneker.Google.SearchIndex/Abstract/IGoogleSearchIndexUtil.cs
+++ src/Soenneker.Google.SearchIndex/Abstract/IGoogleSearchIndexUtil.cs
@@ -9,7 +9,22 @@ namespace Soenneker.Google.SearchIndex.Abstract;
 /// </summary>
 public interface IGoogleSearchIndexUtil
 {
+    /// <summary>
+    /// Adds update index.
+    /// </summary>
+    /// <param name="jobUrl">The job url.</param>
+    /// <param name="action">The action.</param>
+    /// <param name="fileName">The file name.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<PublishUrlNotificationResponse> AddUpdateIndex(string jobUrl, string action, string fileName, CancellationToken cancellationToken = default);
 
+    /// <summary>
+    /// Gets index status.
+    /// </summary>
+    /// <param name="jobUrl">The job url.</param>
+    /// <param name="fileName">The file name.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<UrlNotificationMetadata?> GetIndexStatus(string jobUrl, string fileName, CancellationToken cancellationToken = default);
 }
